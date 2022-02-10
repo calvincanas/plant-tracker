f9cf764510e4a156980a204b4068001b805a8a0a
Calvin Roger S. Canas <calvinrogercanas@gmail.com>
2022-02-10 14:21:45 +0800

    Migration file for creating plants table.

---
+++ database/migrations/2022_02_10_053914_create_plants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('species');
+            $table->text('watering_instructions');
+            $table->string('photo_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plants');
+    }
+};
