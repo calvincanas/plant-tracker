9df229e3cab84d478a65dee6f16906e2ac799685
Calvin Roger S. Canas <calvinrogercanas@gmail.com>
2022-02-12 13:22:21 +0800

    Adjust the test since we use seprate stuff for fe.

--- tests/Feature/Http/Controllers/PlantControllerTest.php
+++ tests/Feature/Http/Controllers/PlantControllerTest.php
@@ -12,13 +12,13 @@ use Tests\TestCase;
 
 class PlantControllerTest extends TestCase
 {
-    Use RefreshDatabase;
+    use RefreshDatabase;
 
     public function test_homepage_all_persisted_plants_show()
     {
         $firstPlant = Plant::factory()->create();
         $secondPlant = Plant::factory()->create();
-        $response = $this->get('/');
+        $response = $this->get('/api/plant');
         $response->assertSee($firstPlant->name);
         $response->assertSee($secondPlant->name);
         $response->assertStatus(200);
@@ -26,7 +26,7 @@ class PlantControllerTest extends TestCase
 
     public function test_homepage_should_not_show_not_existing_plant()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/plant');
         $response->assertDontSee('Plant A');
         $response->assertStatus(200);
     }
@@ -37,7 +37,7 @@ class PlantControllerTest extends TestCase
         Storage::fake('photos');
 
         $photo = UploadedFile::fake()->image('plant1.jpg');
-        $response = $this->post('/plant', [
+        $response = $this->post('/api/plant', [
             'name' => 'Plant A',
             'species' => 'Species A',
             'watering_instructions' => 'Just once a day.',
@@ -47,8 +47,10 @@ class PlantControllerTest extends TestCase
         Storage::disk('local')->assertExists('public/plants/' . $photo->hashName());
         $this->assertDatabaseCount('plants', 1);
 
-        $response->assertSessionHas('success');
-        $response->assertStatus(302);
+        $response->assertJson([
+            'status' => 'success',
+            'message' => 'Created the new plant successfully'
+        ]);
 
         // manual clean up to avoid local disk of dummy image
         @unlink(storage_path('app/public/plants/' . $photo->hashName()));
